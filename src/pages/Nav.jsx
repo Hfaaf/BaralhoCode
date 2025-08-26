362b70c62fa3944a8891c9539fc7ef1db95d7d6e
Hfaaf <heitorfariaspinheirodoamaral@gmail.com>
2025-08-26 11:47:48 -0300

    FixLinkUnlinked

--- src/pages/Nav.jsx
+++ src/pages/Nav.jsx
@@ -4,7 +4,10 @@ export default function Nav() {
     return (
         <nav className="bg-gradient-to-r from-blue-900 via-indigo-900 to-purple-900 shadow-xl rounded-b-3xl w-full mx-auto fixed top-0 left-0 right-0 z-50 border-b border-blue-700">
             <div className="flex flex-col md:flex-row items-center justify-between p-4 max-w-4xl mx-auto">
-                <a href="#" className="flex items-center space-x-3 mb-2 md:mb-0">
+                <a href="https://github.com/Hfaaf/BaralhoCode"
+                   className="flex items-center space-x-3 mb-2 md:mb-0"
+                   target="_blank"
+                >
                     <GiCard10Spades
                         className="h-12 w-12 md:h-14 md:w-14 text-blue-400 hover:animate-spin transition-transform duration-700 ease-in-out drop-shadow-lg"
                         aria-hidden="true"
